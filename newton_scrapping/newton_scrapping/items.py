c53cbb3d613109938685357139a2e4bf3b470088
Subham Polpagedar <subham.p@simformsolutions.com>
2023-03-16 19:24:23 +0530

    Merged with master branch

--- newton_scrapping/newton_scrapping/items.py
+++ newton_scrapping/newton_scrapping/items.py
@@ -8,5 +8,21 @@ import scrapy
 
 class NewtonScrappingItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    titles = scrapy.Field()
+
+    content_type = scrapy.Field()
+    description = scrapy.Field()
+    published_date = scrapy.Field()
+    language = scrapy.Field()
+    text = scrapy.Field()
+    images = scrapy.Field()
+    caption = scrapy.Field()
+    tags = scrapy.Field()
+    author_name = scrapy.Field()
+    publisher_name = scrapy.Field()
+    headline = scrapy.Field()
+    alternativeheadline = scrapy.Field()
+    copyright = scrapy.Field()
+    images_data = scrapy.Field()
+    social_site = scrapy.Field()
+    social_url = scrapy.Field()
