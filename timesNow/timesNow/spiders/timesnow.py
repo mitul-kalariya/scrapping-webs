45d3a26d8e38392dd6fa18283a44b47ea2dbf058
Saunee Navneet <navneet.s@simformsolutions.com>
2023-03-20 18:59:52 +0530

    formatted using flake8

--- timesNow/timesNow/spiders/timesnow.py
+++ timesNow/timesNow/spiders/timesnow.py
@@ -5,16 +5,16 @@ import json
 from datetime import datetime
 from scrapy.selector import Selector
 
-from scrapy.utils.project import get_project_settings
-
 
 class TimesNow(scrapy.Spider):
     name = "times_now_news"
-
     namespace = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9',
                  'news': "http://www.google.com/schemas/sitemap-news/0.9"}
 
-    def __init__(self, type=None, start_date=None, end_date=None, url=None, *args, **kwargs):
+    def __init__(
+        self, type=None, start_date=None,
+        end_date=None, url=None, *args, **kwargs
+                ):
         super(TimesNow, self).__init__(*args, **kwargs)
         self.start_urls = []
         self.articles = []
@@ -28,7 +28,8 @@ class TimesNow(scrapy.Spider):
 
     def parse(self, response):
         """
-        Parses the given `response` object and extracts sitemap URLs or sends a request for articles based on the `type` attribute of the class instance.
+        Parses the given `response` object and extracts sitemap URLs or sends a
+        request for articles based on the `type` attribute of the class instance.
         If `type` is "sitemap", extracts sitemap URLs from the XML content of the response and sends a request for each of them to Scrapy's engine with the callback function `parse_sitemap`.
         If `type` is "articles", sends a request for the given URL to Scrapy's engine with the callback function `parse_article`.
         This function is intended to be used as a Scrapy spider callback function.
@@ -38,8 +39,9 @@ class TimesNow(scrapy.Spider):
         if self.type == "sitemap":
             # mod_date = Selector(response, type='xml').xpath('//sitemap:lastmod/text()',
             #                                                 namespaces=self.namespace).getall()
-            site_map_url = Selector(response, type='xml').xpath('//sitemap:loc/text()',
-                                                                namespaces=self.namespace).getall()
+            site_map_url = Selector(response, type='xml')\
+                            .xpath('//sitemap:loc/text()',
+                                    namespaces=self.namespace).getall()
             for url in site_map_url:
                 yield response.follow(url, callback=self.parse_sitemap)
         elif self.type == "article":
@@ -52,17 +54,22 @@ class TimesNow(scrapy.Spider):
            :param response: the response from the sitemap request
            :return: scrapy.Request object
            """
-        article_urls = Selector(response, type='xml').xpath('//sitemap:loc/text()', namespaces=self.namespace).getall()
-        mod_date = Selector(response, type='xml').xpath('//sitemap:lastmod/text()', namespaces=self.namespace).getall()
+        article_urls = Selector(response, type='xml').\
+            xpath('//sitemap:loc/text()', namespaces=self.namespace).getall()
+        mod_date = Selector(response, type='xml')\
+            .xpath('//sitemap:lastmod/text()',
+                    namespaces=self.namespace).getall()
         try:
             for url, date in zip(article_urls, mod_date):
                 _date = datetime.strptime(date.split("T")[0], '%Y-%m-%d')
                 if self.today_date:
                     if _date == self.today_date:
-                        yield scrapy.Request(url, callback=self.parse_sitemap_article)
+                        yield scrapy.Request(
+                            url, callback=self.parse_sitemap_article)
                 else:
                     if self.start_date <= _date <= self.end_date:
-                        yield from scrapy.Request(url, callback=self.parse_sitemap_article)
+                        yield from scrapy.Request(
+                            url, callback=self.parse_sitemap_article)
         except Exception as e:
             self.logger.exception(f"Error in parse_sitemap:- {e}")
 
@@ -107,10 +114,16 @@ class TimesNow(scrapy.Spider):
         if self.type == "sitemap":
             if not os.path.isdir('Links'):
                 os.makedirs('Links')
-            filename = os.path.join('Links', f'economistnews-sitemap-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+            filename = os.path.join(
+                'Links', f'economistnews-sitemap-\
+                    {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+                )
         elif self.type == "article":
             if not os.path.isdir('Article'):
                 os.makedirs('Article')
-            filename = os.path.join('Article', f'economistnews-articles-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+            filename = os.path.join(
+                'Article', f'economistnews-articles-\
+                    {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+                )
         with open(f'{filename}.json', 'w') as f:
             json.dump(self.articles, f, indent=4)
