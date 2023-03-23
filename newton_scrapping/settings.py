92a4d4d229bfbf00ac6cb6d002a93f9073299adc
Saunee Navneet <navneet.s@simformsolutions.com>
2023-03-23 16:22:52 +0530

    struc according to master

--- newton_scrapping/settings.py
+++ newton_scrapping/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for timesNow project
+# Scrapy settings for newton_scrapping project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = "timesNow"
+BOT_NAME = "newton_scrapping"
 
-SPIDER_MODULES = ["timesNow.spiders"]
-NEWSPIDER_MODULE = "timesNow.spiders"
+SPIDER_MODULES = ["newton_scrapping.spiders"]
+NEWSPIDER_MODULE = "newton_scrapping.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "timesNow (+http://www.yourdomain.com)"
+#USER_AGENT = "newton_scrapping (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    "timesNow.middlewares.TimesnowSpiderMiddleware": 543,
+#    "newton_scrapping.middlewares.NewtonScrappingSpiderMiddleware": 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    "timesNow.middlewares.TimesnowDownloaderMiddleware": 543,
+#    "newton_scrapping.middlewares.NewtonScrappingDownloaderMiddleware": 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    "timesNow.pipelines.TimesnowPipeline": 300,
+#    "newton_scrapping.pipelines.NewtonScrappingPipeline": 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
