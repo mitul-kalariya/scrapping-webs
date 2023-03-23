cc4db0b1c7d856ae75eedf37789d4a5d3d4281a8
Kuldeep Patel <kuldeep.p@simformsolutions.com>
2023-03-23 17:57:52 +0530

    completed refactoring for cbc news website

---
+++ newton_scrapping/spiders/cbc_news.py
@@ -0,0 +1,263 @@
+"""Spider to scrap CBC news website"""
+
+import logging
+from datetime import datetime
+from abc import ABC, abstractmethod
+
+import scrapy
+
+from scrapy.exceptions import CloseSpider
+from scrapy.selector import Selector
+from scrapy.loader import ItemLoader
+
+from newton_scrapping.items import ArticleData
+from newton_scrapping.utils import (
+    based_on_scrape_type,
+    date_in_date_range,
+    get_raw_response,
+    get_parsed_json,
+    export_data_to_json_file,
+    get_parsed_data,
+)
+from newton_scrapping.exceptions import (
+    SitemapScrappingException,
+    SitemapArticleScrappingException,
+    ArticleScrappingException,
+    ExportOutputFileException,
+)
+
+# Setting the threshold of logger to DEBUG
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(name)s] %(levelname)s:   %(message)s",
+    filename="logs.log",
+    filemode="a",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+# Creating an object
+logger = logging.getLogger()
+
+
+class BaseSpider(ABC):
+    @abstractmethod
+    def parse(self, response):
+        pass
+
+    @abstractmethod
+    def parse_sitemap(self, response: str) -> None:
+        pass
+
+    def parse_sitemap_article(self, response: str) -> None:
+        pass
+
+    @abstractmethod
+    def parse_article(self, response: str) -> list:
+        pass
+
+
+class CbcNewsSpider(scrapy.Spider, BaseSpider):
+    """Spider class to scrap sitemap and articles of CBC News site"""
+
+    name = "cbc_news"
+    start_urls = ["http://www.cbc.ca/"]
+
+    def __init__(
+        self, *args, type=None, url=None, start_date=None, end_date=None, **kwargs
+    ):
+        """init method to take date, type and validating it"""
+
+        super(CbcNewsSpider).__init__(*args, **kwargs)
+        try:
+            self.start_urls = []
+            self.articles = []
+            self.date_range_lst = []
+            self.article_url = url
+            self.error_msg_dict = {}
+            self.type = type
+            self.scrape_start_date = (
+                datetime.strptime(start_date, "%Y-%m-%d").date() if start_date else None
+            )
+            self.scrape_end_date = (
+                datetime.strptime(end_date, "%Y-%m-%d").date() if end_date else None
+            )
+
+            self.current_date, self.date_range_lst = based_on_scrape_type(
+                self.type, self.scrape_start_date, self.scrape_end_date, url
+            )
+            if self.current_date:
+                self.scrape_start_date = self.scrape_end_date = self.current_date
+            self.start_urls.append(
+                url if self.type == "article" else "https://www.cbc.ca/rss/"
+            )
+
+        except Exception as exception:
+            self.error_msg_dict["error_msg"] = (
+                "Error occurred while taking type, url, start_date and end_date args. "
+                + str(exception)
+            )
+            self.log(
+                "Error occurred while taking type, url, start_date and end_date args. "
+                + str(exception),
+                level=logging.ERROR,
+            )
+
+    def parse(self, response: str, **kwargs) -> None:
+        """differentiate sitemap and article and redirect its callback to different parser
+        Args:
+            response: generated response
+        Raises:
+            CloseSpider: Close spider if error in passed args
+            Error if any while scrapping
+        Returns:
+            None
+        """
+        if self.error_msg_dict:
+            raise CloseSpider(self.error_msg_dict.get("error_msg"))
+
+        if response.status != 200:
+            raise CloseSpider(
+                f"Unable to scrape due to getting this status code {response.status}"
+            )
+
+        if "rss" in response.url:
+            for link in response.css("td.content a::attr(href)").getall():
+                try:
+                    yield scrapy.Request(
+                        link,
+                        callback=self.parse_sitemap,
+                    )
+                except Exception as exception:
+                    self.log(
+                        f"Error occured while iterating sitemap url. {str(exception)}",
+                        level=logging.ERROR,
+                    )
+        else:
+            yield self.parse_article(response)
+
+    def parse_sitemap(self, response: str) -> None:
+        """
+        parse sitemap from sitemap url and callback parser to parse title and link
+        Args:
+            response: generated response
+        Raises:
+            ValueError if not provided
+        Returns:
+            Values of parameters
+        """
+
+        for url, date in zip(
+            Selector(response, type="xml").xpath("//item/link/text()").getall(),
+            Selector(response, type="xml").xpath("//item/pubDate/text()").getall(),
+        ):
+            try:
+                date_datetime = datetime.strptime(
+                    date[:-3].strip(), "%a, %d %b %Y %H:%M:%S"
+                )
+                if date_in_date_range(date_datetime, self.date_range_lst):
+                    yield scrapy.Request(
+                        url.strip(), callback=self.parse_sitemap_article
+                    )
+            except Exception as exception:
+                self.log(
+                    f"Error occurred while fetching sitemap:- {str(exception)}",
+                    level=logging.ERROR,
+                )
+                raise SitemapScrappingException(
+                    f"Error occurred while fetching sitemap:- {str(exception)}"
+                ) from exception
+
+    def parse_sitemap_article(self, response: str) -> None:
+        """
+        parse sitemap article and scrap title and link
+        Args:
+            response: generated response
+        Raises:
+            ValueError if not provided
+        Returns:
+            Values of parameters
+        """
+        try:
+            if title := response.css("h1.detailHeadline::text").get():
+                data = {"link": response.url.replace("?cmp=rss", ""), "title": title}
+                self.articles.append(data)
+        except Exception as exception:
+            self.log(
+                f"Error occurred while fetching article details from sitemap:- {str(exception)}",
+                level=logging.ERROR,
+            )
+            raise SitemapArticleScrappingException(
+                f"Error occurred while fetching article details from sitemap:- {str(exception)}"
+            ) from exception
+
+    def parse_article(self, response: str) -> None:
+        """
+        parse article and append related data to class's articles variable
+        Args:
+            response: generated response
+        Raises:
+            ValueError if not provided
+        Returns:
+            Values of parameters
+        """
+
+        try:
+            raw_response_dict = {
+                "content_type": response.headers.get("Content-Type").decode("utf-8"),
+                "content": response.text,
+            }
+            raw_response = get_raw_response(response, raw_response_dict)
+            articledata_loader = ItemLoader(item=ArticleData(), response=response)
+            parsed_json_dict = {}
+
+            parsed_json_main = response.css('script[type="application/ld+json"]::text')
+            parsed_json_misc = response.css('script[type="application/json"]::text')
+
+            if parsed_json_main:
+                parsed_json_dict["main"] = parsed_json_main
+            if parsed_json_misc:
+                parsed_json_dict["misc"] = parsed_json_misc
+
+            parsed_json_data = get_parsed_json(response, parsed_json_dict)
+            articledata_loader.add_value("raw_response", raw_response)
+            if parsed_json_data:
+                articledata_loader.add_value(
+                    "parsed_json",
+                    parsed_json_data,
+                )
+            articledata_loader.add_value(
+                "parsed_data", get_parsed_data(response, parsed_json_main)
+            )
+
+            self.articles.append(dict(articledata_loader.load_item()))
+        except Exception as exception:
+            self.log(
+                f"Error occurred while fetching article details:- {str(exception)}",
+                level=logging.ERROR,
+            )
+            raise ArticleScrappingException(
+                f"Error occurred while fetching article details:-  {str(exception)}"
+            ) from exception
+
+    def closed(self, reason: any) -> None:
+        """
+        store all scrapped data into json file with given date in filename
+        Args:
+            reason: generated reason
+        Raises:
+            ValueError if not provided
+        Returns:
+            Values of parameters
+        """
+        try:
+            if not self.articles:
+                self.log("No articles or sitemap url scrapped.", level=logging.INFO)
+            else:
+                export_data_to_json_file(self.type, self.articles, self.name)
+        except Exception as exception:
+            self.log(
+                f"Error occurred while exporting file:- {str(exception)} - {reason}",
+                level=logging.ERROR,
+            )
+            raise ExportOutputFileException(
+                f"Error occurred while exporting file:- {str(exception)} - {reason}"
+            ) from exception
---
+++ newton_scrapping/utils.py
@@ -0,0 +1,453 @@
+""" General functions """
+from datetime import timedelta, datetime
+import itertools
+import json
+import os
+
+
+from scrapy.loader import ItemLoader
+
+from newton_scrapping.items import (
+    ArticleRawResponse,
+    ArticleRawParsedJson,
+)
+from .exceptions import (
+    InputMissingException,
+    InvalidDateException,
+    InvalidArgumentException,
+)
+
+ERROR_MESSAGES = {
+    "InputMissingException": "'{}' field is required.",
+    "InvalidDateException": "Please provide valid date.",
+    "InvalidArgumentException": "Please provide a valid arguments.",
+}
+
+
+def sitemap_validations(
+    scrape_start_date: datetime, scrape_end_date: datetime, article_url: str
+) -> datetime:
+    """
+    Validate the sitemap arguments
+    Args:
+        scrape_start_date (datetime): scrapping start date
+        scrape_end_date (datetime): scrapping end date
+        article_url (str): article url
+    Returns:
+        date: return current date if user not passed any date parameter
+    """
+    if scrape_start_date and scrape_end_date:
+        validate_arg(InvalidDateException, not scrape_start_date > scrape_end_date)
+        validate_arg(
+            InvalidDateException,
+            int((scrape_end_date - scrape_start_date).days) <= 30,
+        )
+    else:
+        validate_arg(
+            InputMissingException,
+            not (scrape_start_date or scrape_end_date),
+            "start_date and end_date",
+        )
+        scrape_start_date = scrape_end_date = datetime.now().date()
+
+    validate_arg(
+        InvalidArgumentException, not article_url, "url is not required for sitemap."
+    )
+
+    return scrape_start_date, scrape_end_date
+
+
+def article_validations(
+    article_url: str, scrape_start_date: datetime, scrape_end_date: datetime
+) -> None:
+    """
+    Validate the article arguments
+
+    Args:
+        article_url (str): article url
+        scrape_start_date (datetime): scrapping start date
+        scrape_end_date (datetime): scrapping end date
+    Returns:
+        None
+    """
+
+    validate_arg(InputMissingException, article_url, "url")
+    validate_arg(
+        InvalidArgumentException,
+        not (scrape_start_date or scrape_end_date),
+        "start_date and end_date argument is not required for article.",
+    )
+
+
+def date_range(start_date: datetime, end_date: datetime) -> None:
+    """
+    Return range of all date between given date
+    if not end_date then take start_date as end date
+
+    Args:
+        start_date (datetime): scrapping start date
+        end_date (datetime): scrapping end date
+    Returns:
+        Value of parameter
+    """
+    for date in range(int((end_date - start_date).days) + 1):
+        yield start_date + timedelta(date)
+
+
+def date_in_date_range(published_date, date_range_lst):
+    """
+    return true if date is in given start date and end date range
+    """
+    return published_date.date() in date_range_lst
+
+
+def validate_arg(param_name, param_value, custom_msg=None) -> None:
+    """
+    Validate the param.
+
+    Args:
+        param_name: Name of the parameter to be validated
+        param_value: Value of the required parameter
+
+    Raises:
+        ValueError if not provided
+    Returns:
+          Value of parameter
+    """
+    if not param_value:
+        raise param_name(f"{ERROR_MESSAGES[param_name.__name__]} {custom_msg}")
+
+
+def based_on_scrape_type(
+    scrape_type: str, scrape_start_date: datetime, scrape_end_date: datetime, url: str
+) -> datetime:
+    """
+    check scrape type and based on the type pass it to the validated function,
+    after validation return required values.
+
+     Args:
+         scrape_type: Name of the scrape type
+         scrape_start_date (datetime): scrapping start date
+         scrape_end_date (datetime): scrapping end date
+         url: url to be used
+
+     Returns:
+         datetime: if scrape_type is sitemap
+         list: if scrape_type is sitemap
+    """
+    if scrape_type == "article":
+        article_validations(url, scrape_start_date, scrape_end_date)
+        return None, None
+    if scrape_type == "sitemap":
+        scrape_start_date, scrape_end_date = sitemap_validations(
+            scrape_start_date, scrape_end_date, url
+        )
+        date_range_lst = []
+        date_range_lst.extend(iter(date_range(scrape_start_date, scrape_end_date)))
+        return scrape_start_date, date_range_lst
+
+    return validate_arg("MISSING_REQUIRED_FIELD", None, "type")
+
+
+def get_raw_response(response: str, selector_and_key: dict) -> dict:
+    """
+    Raw response data generated from given response and selector
+
+    Args:
+        response: provided response
+        selector_and_key: A dictionary with key and selector
+
+    Returns:
+        Dictionary with generated raw response
+    """
+    article_raw_response_loader = ItemLoader(
+        item=ArticleRawResponse(), response=response
+    )
+    for key, value in selector_and_key.items():
+        article_raw_response_loader.add_value(key, value)
+    return dict(article_raw_response_loader.load_item())
+
+
+def get_parsed_json(response: str, selector_and_key: dict) -> dict:
+    """
+     Parsed json response from generated data using given response and selector
+
+    Args:
+        response: provided response
+        selector_and_key: A dictionary with key and selector
+
+    Returns:
+        Dictionary with Parsed json response from generated data
+    """
+    article_raw_parsed_json_loader = ItemLoader(
+        item=ArticleRawParsedJson(), response=response
+    )
+
+    for key, value in selector_and_key.items():
+        article_raw_parsed_json_loader.add_value(
+            key, [json.loads(data) for data in value.getall()]
+        )
+    return dict(article_raw_parsed_json_loader.load_item())
+
+
+def export_data_to_json_file(scrape_type: str, file_data: str, file_name: str) -> None:
+    """
+    Export data to json file
+
+    Args:
+        scrape_type: Name of the scrape type
+        file_data: file data
+        file_name: Name of the file which contain data
+
+    Raises:
+        ValueError if not provided
+
+    Returns:
+        Values of parameters
+    """
+    folder_structure = ""
+    if scrape_type == "sitemap":
+        folder_structure = "Links"
+        filename = (
+            f'{file_name}-sitemap-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json'
+        )
+
+    elif scrape_type == "article":
+        folder_structure = "Article"
+        filename = (
+            f'{file_name}-articles-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json'
+        )
+
+    if not os.path.exists(folder_structure):
+        os.makedirs(folder_structure)
+
+    with open(f"{folder_structure}/{filename}", "w", encoding="utf-8") as file:
+        json.dump(file_data, file, indent=4)
+
+
+def get_parsed_data_dict() -> dict:
+    """
+    Return base data dictionary
+
+    Args:
+    None
+
+    Returns:
+        dict: Return base data dictionary
+    """
+    return {
+        "country": None,
+        "language": None,
+        "author": [{"@type": None, "name": None, "url": None}],
+        "description": None,
+        "modified_at": None,
+        "published_at": None,
+        "publisher": None,
+        "text": None,
+        "thumbnail_image": None,
+        "title": None,
+        "images": None,
+        "section": None,
+        "video": None,
+    }
+
+
+def remove_empty_elements(parsed_data_dict: dict) -> dict:
+    """
+    Recursively remove empty lists, empty dicts, or None elements from a dictionary.
+    :param parsed_data_dict: Input dictionary.
+    :type parsed_data_dict: dict
+    :return: Dictionary with all empty lists, and empty dictionaries removed.
+    :rtype: dict
+    """
+
+    def empty(value):
+        return value is None or value == {} or value == []
+
+    if not isinstance(parsed_data_dict, (dict, list)):
+        data_dict = parsed_data_dict
+    elif isinstance(parsed_data_dict, list):
+        data_dict = [
+            value
+            for value in (remove_empty_elements(value) for value in parsed_data_dict)
+            if not empty(value)
+        ]
+    else:
+        data_dict = {
+            key: value
+            for key, value in (
+                (key, remove_empty_elements(value))
+                for key, value in parsed_data_dict.items()
+            )
+            if not empty(value)
+        }
+    return data_dict
+
+
+def get_parsed_data(response: str, parsed_json_main: list) -> dict:
+    """
+     Parsed data response from generated data using given response and selector
+
+    Args:
+        response: provided response
+        parsed_json_main: A list of dictionary with applications/+ld data
+
+    Returns:
+        Dictionary with Parsed json response from generated data
+    """
+    parsed_data_dict = get_parsed_data_dict()
+    parsed_data_dict |= {
+        "country": ["Canada"],
+        "language": [response.css("html::attr(lang)").get()],
+    }
+    parsed_data_dict |= get_author_details(parsed_json_main.getall(), response)
+    parsed_data_dict |= get_descriptions_date_details(parsed_json_main.getall())
+    parsed_data_dict |= get_publihser_details(parsed_json_main.getall(), response)
+    parsed_data_dict |= get_text_title_section_details(
+        parsed_json_main.getall(), response
+    )
+    parsed_data_dict |= get_thumbnail_image_video(parsed_json_main.getall(), response)
+    return remove_empty_elements(parsed_data_dict)
+
+
+def get_author_details(parsed_data: list, response: str) -> dict:
+    """
+    Return author related details
+    Args:
+        parsed_data: response of application/ld+json data
+        response: provided response
+    Returns:
+        dict: author related details
+    """
+    return next(
+        (
+            {
+                "author": [
+                    {
+                        "@type": json.loads(block).get("author")[0].get("@type"),
+                        "name": json.loads(block).get("author")[0].get("name"),
+                        "url": json.loads(block)
+                        .get("author")[0]
+                        .get("image", None)
+                        .get("url", None),
+                    }
+                ]
+            }
+            for block in parsed_data
+            if json.loads(block).get("author")
+        ),
+        {
+            "author": [
+                {"name": response.css("#detailContent > div.byline > div::text").get()}
+            ]
+        },
+    )
+
+
+def get_descriptions_date_details(parsed_data: list) -> dict:
+    """
+    Returns description, modified date, published date details
+    Args:
+        parsed_data: response of application/ld+json data
+    Returns:
+        dict: description, modified date, published date related details
+    """
+    return next(
+        (
+            {
+                "description": [json.loads(block).get("description")],
+                "modified_at": [json.loads(block).get("dateModified")],
+                "published_at": [json.loads(block).get("datePublished")],
+            }
+            for block in parsed_data
+            if "NewsArticle" in json.loads(block).get("@type")
+        ),
+        {
+            "description": None,
+            "modified_at": None,
+            "published_at": None,
+        },
+    )
+
+
+def get_publihser_details(parsed_data: list, response: str) -> dict:
+    """
+    Returns publisher details like name, type, id
+    Args:
+        parsed_data: response of application/ld+json data
+        response: provided response
+    Returns:
+        dict: publisher details like name, type, id related details
+    """
+    for block in parsed_data:
+        return {
+            "publisher": [
+                {
+                    "@id": response.css("#menuButton::attr(href)")
+                    .get()
+                    .split("/sitemap")[0][2:],
+                    "@type": json.loads(block).get("publisher", {}).get("@type"),
+                    "name": response.css("head > title::text").get().split("|")[1],
+                }
+            ]
+        }
+
+
+def get_text_title_section_details(parsed_data: list, response: str) -> dict:
+    """
+    Returns text, title, section details
+    Args:
+        parsed_data: response of application/ld+json data
+        response: provided response
+    Returns:
+        dict: text, title, section details
+    """
+    for block in parsed_data:
+        return {
+            "title": [json.loads(block).get("headline")],
+            "text": ["".join(response.css(".story p::text").getall())],
+            "section": [json.loads(block).get("articleSection")],
+        }
+
+
+def get_thumbnail_image_video(parsed_data: list, response: str) -> dict:
+    """
+    Returns thumbnail images, images and video details
+    Args:
+        parsed_data: response of application/ld+json data
+        response: provided response
+    Returns:
+        dict: thumbnail images, images and video details
+    """
+    image_url = response.css(".storyWrapper .placeholder img::attr(src)").getall()
+    image_caption = [
+        caption.strip()
+        for caption in response.css("figcaption::text").getall()
+        if caption.strip()
+    ]
+    image_caption = [
+        caption_one + caption_two
+        for caption_one, caption_two in zip(image_caption[::2], image_caption[1::2])
+    ]
+    video_caption = None
+    video_url = None
+    thumbnail_url = None
+    for block in parsed_data:
+        if json.loads(block).get("video", None):
+            video_caption = (
+                json.loads(block).get("video", None)[0].get("alternativeHeadline", None)
+            )
+            video_url = json.loads(block).get("video", None)[0].get("contentUrl", None)
+        if json.loads(block).get("thumbnailUrl", None):
+            thumbnail_url = json.loads(block).get("thumbnailUrl")
+
+    return {
+        "images": [
+            {"link": img, "caption": cap}
+            for img, cap in itertools.zip_longest(
+                image_url, image_caption, fillvalue=None
+            )
+        ],
+        "video": [{"link": video_url, "caption": video_caption}],
+        "thumbnail_image": [thumbnail_url],
+    }
