42bc8dbea34278ca228c00e3f8880d99eb046977
Subham Polpagedar <subham.p@simformsolutions.com>
2023-03-25 00:45:09 +0530

    Updated test cases as per the package change

--- crwcbcnews/test/helpers/constant.py
+++ crwcbcnews/test/helpers/constant.py
@@ -4,15 +4,15 @@
 TEST_ARTICLES = [
     {
         "url": "https://www.cbc.ca/news/politics/joe-biden-official-visit-canada-1.6789140",
-        "test_data_path": "newton_scrapping/test/data/test_article_1.json"
+        "test_data_path": "crwcbcnews/test/data/test_article_1.json"
     },
     {
         "url": "https://www.cbc.ca/news/canada/saskatchewan/first-person-tenille-campbell-beadwork-1.6749167",
-        "test_data_path": "newton_scrapping/test/data/test_article_2.json"
+        "test_data_path": "crwcbcnews/test/data/test_article_2.json"
     },
     {
         "url": "https://www.cbc.ca/news/canada/north/behchoko-tlicho-youth-wellness-gathering-1.6788711",
-        "test_data_path": "newton_scrapping/test/data/test_article_3.json"
+        "test_data_path": "crwcbcnews/test/data/test_article_3.json"
     }
 ]
 
