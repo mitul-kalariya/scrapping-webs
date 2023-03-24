3ab01b7dd5c16245b1f27793c4908cf540034699
Subham Polpagedar <subham.p@simformsolutions.com>
2023-03-24 17:43:15 +0530

    Added test data

--- newton_scrapping/test/helpers/constant.py
+++ newton_scrapping/test/helpers/constant.py
@@ -3,21 +3,17 @@
 # Update below dictionary to add or remove new articles to test
 TEST_ARTICLES = [
     {
-        "url": "https://indianexpress.com/article/cities/delhi/delhi-hc-releases-lawyer-who-got-6-months-jail-for-not-paying-rs-32-lakh-rent-after-settlement-8511700/",
+        "url": "https://www.timesnownews.com/business-economy/companies/tiktok-can-still-access-years-of-indians-data-despite-ban-this-is-ceo-shou-zi-chews-response-when-grilled-by-the-us-congress-article-98955792",
         "test_data_path": "newton_scrapping/test/data/test_article_1.json"
     },
     {
-        "url": "https://indianexpress.com/article/education/qs-world-university-rankings-by-subject-du-iit-delhi-mumbai-iisc-courses-8512300/",
+        "url": "https://www.timesnownews.com/entertainment-news/amid-dating-rumours-aditya-roy-kapur-spills-beans-on-his-wedding-plans-i-will-take-my-time-bollywood-news-article-98955563",
         "test_data_path": "newton_scrapping/test/data/test_article_2.json"
     },
-    # {
-    #     "url": "https://indianexpress.com/article/education/qs-world-university-rankings-by-subject-du-iit-delhi-mumbai-iisc-courses-8512300/",
-    #     "test_data_path": "newton_scrapping/test/data/test_article_2.json"
-    # },
-    # {
-    #     "url": "https://indianexpress.com/article/sports/cricket/david-still-has-a-burning-desire-to-open-the-batting-for-australia-candice-warner-8512422/",
-    #     "test_data_path": "newton_scrapping/test/data/test_article_3.json"
-    # }
+    {
+        "url": "https://www.timesnownews.com/sports/cricket/warner-to-lead-no-rishabh-pant-shardul-traded-complete-list-of-changes-in-delhi-capitals-for-ipl-2023-article-98947626",
+        "test_data_path": "newton_scrapping/test/data/test_article_3.json"
+    },
 ]
 
 SITEMAP_URL = "https://www.timesnownews.com/staticsitemap/timesnow/sitemap-index.xml"
