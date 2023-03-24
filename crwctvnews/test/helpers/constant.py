13f97fd15e45bc3618392da34f145f2a62ccac86
Subham Polpagedar <subham.p@simformsolutions.com>
2023-03-25 00:27:42 +0530

    Updated test cases as per package changes

--- crwctvnews/test/helpers/constant.py
+++ crwctvnews/test/helpers/constant.py
@@ -4,15 +4,15 @@
 TEST_ARTICLES = [
     {
         "url": "https://www.ctvnews.ca/sci-tech/utah-bans-kids-from-accessing-social-media-during-evening-hours-without-parent-consent-1.6327047",
-        "test_data_path": "newton_scrapping/test/data/test_article_1.json"
+        "test_data_path": "crwctvnews/test/data/test_article_1.json"
     },
     {
         "url": "https://www.ctvnews.ca/politics/restaurants-and-bars-brace-for-biggest-alcohol-tax-jump-in-40-years-1.6327044",
-        "test_data_path": "newton_scrapping/test/data/test_article_2.json"
+        "test_data_path": "crwctvnews/test/data/test_article_2.json"
     },
     {
         "url": "https://www.ctvnews.ca/politics/norad-haiti-migration-critical-minerals-to-top-agenda-for-trudeau-and-biden-1.6327052",
-        "test_data_path": "newton_scrapping/test/data/test_article_3.json"
+        "test_data_path": "crwctvnews/test/data/test_article_3.json"
     }
 ]
 
