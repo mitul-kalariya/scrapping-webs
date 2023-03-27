a5d8fa4f71705c608450500b57904af6517ec445
Kuldeep Patel <kuldeep.p@simformsolutions.com>
2023-03-27 17:03:59 +0530

    BLOCKER - Scraping sitemap

---
+++ newton_scrapping/spiders/tvanouvelles.py
@@ -0,0 +1,259 @@
+"""Spider to scrap TVA news website"""
+
+import logging
+from datetime import datetime
+from abc import ABC, abstractmethod
+
+import scrapy
+
+from scrapy.exceptions import CloseSpider
+from scrapy.selector import Selector
+from scrapy.loader import ItemLoader
+
+from newton_scrapping.items import ArticleData
+from newton_scrapping.utils import (
+    based_on_scrape_type,
+    date_range,
+    date_in_date_range,
+    get_raw_response,
+    get_parsed_json,
+    export_data_to_json_file,
+    get_parsed_data,
+    remove_empty_elements,
+)
+from newton_scrapping.exceptions import (
+    SitemapScrappingException,
+    SitemapArticleScrappingException,
+    ArticleScrappingException,
+    ExportOutputFileException,
+)
+
+# Setting the threshold of logger to DEBUG
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(name)s] %(levelname)s:   %(message)s",
+    filename="logs.log",
+    filemode="a",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+# Creating an object
+logger = logging.getLogger()
+
+
+class BaseSpider(ABC):
+    @abstractmethod
+    def parse(self, response):
+        pass
+
+    @abstractmethod
+    def parse_sitemap(self, response: str) -> None:
+        pass
+
+    def parse_sitemap_article(self, response: str) -> None:
+        pass
+
+    @abstractmethod
+    def parse_article(self, response: str) -> list:
+        pass
+
+
+class TvanouvellesSpider(scrapy.Spider, BaseSpider):
+    """Spider class to scrap sitemap and articles of TVA News site"""
+
+    name = "tvanouvelles"
+    start_urls = ["http://www.tvanouvelles.ca/"]
+    namespace = {"sitemap": "https://www.sitemaps.org/schemas/sitemap/0.9"}
+    namespace_news = {"sitemap": "https://www.google.com/schemas/sitemap-news/0.9"}
+
+    def __init__(
+        self, *args, type=None, url=None, start_date=None, end_date=None, **kwargs
+    ):
+        """init method to take date, type and validating it"""
+
+        super(TvanouvellesSpider).__init__(*args, **kwargs)
+        try:
+            self.start_urls = []
+            self.articles = []
+            self.date_range_lst = []
+            self.article_url = url
+            self.error_msg_dict = {}
+            self.type = type
+            self.scrape_start_date = (
+                datetime.strptime(start_date, "%Y-%m-%d").date() if start_date else None
+            )
+            self.scrape_end_date = (
+                datetime.strptime(end_date, "%Y-%m-%d").date() if end_date else None
+            )
+
+            self.current_date, self.date_range_lst = based_on_scrape_type(
+                self.type, self.scrape_start_date, self.scrape_end_date, url
+            )
+            if self.current_date:
+                self.scrape_start_date = self.scrape_end_date = self.current_date
+            self.start_urls.append(
+                url
+                if self.type == "article"
+                else "https://www.tvanouvelles.ca/news.xml"
+            )
+
+        except Exception as exception:
+            self.error_msg_dict["error_msg"] = (
+                "Error occurred while taking type, url, start_date and end_date args. "
+                + str(exception)
+            )
+            self.log(
+                "Error occurred while taking type, url, start_date and end_date args. "
+                + str(exception),
+                level=logging.ERROR,
+            )
+
+    def parse(self, response: str, **kwargs) -> None:
+        """
+        differentiate sitemap and article and redirect its callback to different parser
+        Args:
+            response: generated response
+        Raises:
+            CloseSpider: Close spider if error in passed args
+            Error if any while scrapping
+        Returns:
+            None
+        """
+        if self.error_msg_dict:
+            raise CloseSpider(self.error_msg_dict.get("error_msg"))
+        if response.status != 200:
+            raise CloseSpider(
+                f"Unable to scrape due to getting this status code {response.status}"
+            )
+
+        if "news.xml" in response.url:
+            yield scrapy.Request(response.url, callback=self.parse_sitemap)
+        else:
+            yield self.parse_article(response)
+
+    def parse_sitemap(self, response: str) -> None:
+        """
+        parse sitemap and scrap article url
+        Args:
+            response: generated response
+        Raises:
+            ValueError if not provided
+        Returns:
+            Values of parameters
+        """
+        for link, date in zip(
+            Selector(response, type="xml")
+            .xpath("//sitemap:loc/text()", namespaces=self.namespace)
+            .getall(),
+            Selector(response, type="xml")
+            .xpath("//sitemap:publication_date/text()", namespaces=self.namespace_news)
+            .getall(),
+        ):
+            try:
+                date_datetime_obj = datetime.strptime(
+                    date.strip()[:-6], "%Y-%m-%dT%H:%M:%S"
+                )
+                if date_in_date_range(date_datetime_obj, self.date_range_lst):
+                    yield scrapy.Request(
+                        link.strip(), callback=self.parse_sitemap_article
+                    )
+            except Exception as exception:  # pylint: disable=broad-except
+                self.log(
+                    f"Error occurred while fetching sitemap:- {str(exception)}",
+                    level=logging.ERROR,
+                )
+                raise SitemapScrappingException(
+                    f"Error occurred while fetching sitemap:- {str(exception)}"
+                ) from exception
+
+    def parse_sitemap_article(self, response: str) -> None:
+        """
+        parse sitemap article and scrap title and link
+        Args:
+            response: generated response
+        Raises:
+            ValueError if not provided
+        Returns:
+            Values of parameters
+        """
+        try:
+            print("response===============", response.url)
+            if title := response.css(".story-title h1::text").get():
+                data = {"link": response.url, "title": title}
+                self.articles.append(data)
+        except Exception as exception:
+            self.log(
+                f"Error occurred while fetching article details from sitemap:- {str(exception)}",
+                level=logging.ERROR,
+            )
+            raise SitemapArticleScrappingException(
+                f"Error occurred while fetching article details from sitemap:- {str(exception)}"
+            ) from exception
+
+    def parse_article(self, response: str) -> None:
+        """
+        parse article and append related data to class's articles variable
+        Args:
+            response: generated response
+        Raises:
+            ValueError if not provided
+        Returns:
+            Values of parameters
+        """
+        try:
+            raw_response_dict = {
+                "content_type": response.headers.get("Content-Type").decode("utf-8"),
+                "content": response.text,
+            }
+            raw_response = get_raw_response(response, raw_response_dict)
+            articledata_loader = ItemLoader(item=ArticleData(), response=response)
+
+            parsed_json_main = response.css('script[type="application/ld+json"]::text')
+
+            parsed_json_data = get_parsed_json(response)
+            articledata_loader.add_value("raw_response", raw_response)
+            if parsed_json_data:
+                articledata_loader.add_value(
+                    "parsed_json",
+                    parsed_json_data,
+                )
+            articledata_loader.add_value(
+                "parsed_data", get_parsed_data(response, parsed_json_main)
+            )
+
+            self.articles.append(
+                remove_empty_elements(dict(articledata_loader.load_item()))
+            )
+
+        except Exception as exception:
+            self.log(
+                f"Error occurred while scrapping an article for this link {response.url}."
+                + str(exception),
+                level=logging.ERROR,
+            )
+            raise ArticleScrappingException(
+                f"Error occurred while fetching article details:-  {str(exception)}"
+            ) from exception
+
+    def closed(self, reason: any) -> None:
+        """
+        store all scrapped data into json file with given date in filename
+        Args:
+            reason: generated reason
+        Raises:
+            ValueError if not provided
+        Returns:
+            Values of parameters
+        """
+        try:
+            if not self.articles:
+                self.log("No articles or sitemap url scrapped.", level=logging.INFO)
+            else:
+                export_data_to_json_file(self.type, self.articles, self.name)
+        except Exception as exception:
+            self.log(
+                f"Error occurred while exporting file:- {str(exception)} - {reason}",
+                level=logging.ERROR,
+            )
+            raise ExportOutputFileException(
+                f"Error occurred while exporting file:- {str(exception)} - {reason}"
+            ) from exception
