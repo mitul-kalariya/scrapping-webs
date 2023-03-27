f344dd8d5e13ef77d0c4f44e7e95adc107e1a7eb
Mihir Vora <mihir.v@simformsolutions.com>
2023-03-27 13:00:21 +0530

    change in ard news

--- newton_scrapping/utils.py
+++ newton_scrapping/utils.py
@@ -21,8 +21,8 @@ def create_log_file():
 
 
 def validate_sitemap_date_range(start_date, end_date):
-    start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
-    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+    start_date = datetime.strptime(start_date, "%Y-%m-%d").date() if start_date else None
+    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()  if end_date else None
     try:
         if start_date and not end_date:
             raise exceptions.InvalidDateException(
