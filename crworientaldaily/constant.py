7c3d8591cac668eef8425605c4647477fe81db58
siddharth asodariya <siddharth.a@simformsolutions.com>
2023-03-28 18:09:16 +0530

    added second sitemap url

--- crworientaldaily/constant.py
+++ crworientaldaily/constant.py
@@ -1,5 +1,7 @@
 """constants"""
 
 SITEMAP_URL = "https://www.orientaldaily.com.my/sitemap.xml"
+# "https://orientaldaily.on.cc/sitemap.xml"
+
 # In case when sitemap, RSS feed or archive is not available.
 BASE_URL = "https://www.orientaldaily.com.my/"
