40cb7ae63e2d10e7c669cb2d3a5dd5c80edc56fa
mitul-rathod <mitul.r@simformsolutions.com>
2023-03-28 22:28:20 +0530

    Added : Final Changes for Package

--- crwbfmtv/spiders/bfm_tv.py
+++ crwbfmtv/spiders/bfm_tv.py
@@ -229,8 +229,8 @@ class BFMTVSpider(scrapy.Spider, BaseSpider):
                 self.output_callback(self.articles)
             if not self.articles:
                 self.log("No articles or sitemap url scrapped.", level=logging.INFO)
-            else:
-                export_data_to_json_file(self.type, self.articles, self.name)
+            # else:
+            #     export_data_to_json_file(self.type, self.articles, self.name)
         except Exception as exception:
             exceptions.ExportOutputFileException(
                 f"Error occurred while writing json file{str(exception)} - {reason}"
--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         'scrapy',
+        'selenium',
+        'webdriver-manager',
+        'beautifulsoup4',
     ],
 )
