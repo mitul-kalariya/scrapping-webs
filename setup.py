24fa881fb762c9049f224891a904a2c54fe590a6
Subham Polpagedar <subham.p@simformsolutions.com>
2023-03-29 19:25:14 +0530

    Updated requirements.txt and setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='crwsueddeutsche',
     author='Newton',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'scrapy',
-    ],
+    install_requires=requirements,
 )
