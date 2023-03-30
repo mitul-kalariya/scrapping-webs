c4e4deb761545055c0da188913b4cacb4b0b64ed
siddharth asodariya <siddharth.a@simformsolutions.com>
2023-03-30 11:24:22 +0530

    removed generating json file

--- crworientaldaily/spiders/oriental_daily.py
+++ crworientaldaily/spiders/oriental_daily.py
@@ -16,7 +16,7 @@ from crworientaldaily.utils import (
     validate,
     get_raw_response,
     get_parsed_json,
-    export_data_to_json_file,
+    # export_data_to_json_file,
     get_parsed_data,
     remove_empty_elements,
 )
@@ -246,8 +246,8 @@ class OrientalDailySpider(scrapy.Spider, BaseSpider):
                 self.output_callback(self.articles)
             if not self.articles:
                 self.log("No articles or sitemap url scrapped.", level=logging.INFO)
-            else:
-                export_data_to_json_file(self.type, self.articles, self.name)
+            # else:
+            #     export_data_to_json_file(self.type, self.articles, self.name)
         except Exception as exception:
             self.log(
                 f"Error occurred while exporting file:- {str(exception)} - {reason}",
