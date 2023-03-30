f85e022e7caad72b19e68b514fd213c2fc9e6479
Mihir Vora <mihir.v@simformsolutions.com>
2023-03-30 13:19:06 +0530

    added: constants.py files

--- newton_scrapping/constant.py
+++ newton_scrapping/constant.py
@@ -1,5 +1,9 @@
 """constants"""
 
-SITEMAP_URL = ""
-# In case when sitemap, RSS feed or archive is not available.
-BASE_URL = ""
+from datetime import datetime
+import logging
+
+TODAYS_DATE = datetime.today().date()
+BASE_URL = "https://www.tagesschau.de"
+SITEMAP_URL = "https://www.tagesschau.de/archiv/"
+LOGGER = logging.getLogger()
