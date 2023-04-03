8a4bdaec2ddd8a1ac666649cee00f8c8d7277ba6
Hariom Suryvanshi <hariom.suryvanshi@ivanti.com>
2023-04-03 19:50:39 +0530

    did some changes

--- crwnhkorjp/test/helpers/constant.py
+++ crwnhkorjp/test/helpers/constant.py
@@ -2,43 +2,43 @@
 
 TEST_ARTICLES = [
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014025081000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027131000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_1.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014024841000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027091000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_2.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014025021000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027341000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_3.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014020761000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027351000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_4.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014025251000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027411000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_5.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014025321000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027391000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_6.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014025291000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027471000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_7.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014025271000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027501000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_8.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014025201000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230403/k10014027561000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_9.json"
     },
     {
-        "url": "https://www3.nhk.or.jp/news/html/20230331/k10014025221000.html",
+        "url": "https://www3.nhk.or.jp/news/html/20230402/k10014027241000.html",
         "test_data_path": "crwnhkorjp/test/data/test_article_10.json"
     }
     
