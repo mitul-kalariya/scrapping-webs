9fec55e140e4de6c0fe6b4ba452a885906f5e5cd
Subham Polpagedar <subham.p@simformsolutions.com>
2023-04-04 19:58:58 +0530

    moved scrapper.py in root

---
+++ scrapper.py
@@ -0,0 +1,11 @@
+from crwmbcnews import Crawler
+
+crawler = Crawler(query={"type": "sitemap"})
+links = crawler.crawl()
+
+for link in links[:5]:
+    article = Crawler(query={"type": "article", "link": link["link"]})
+    data = article.crawl()
+    print(data)
+    print("----------------------------------------------------")
+
