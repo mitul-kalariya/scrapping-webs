6e64b7520832a64e82209abd5ab9188142c2eb7a
Mihir Vora <mihir.v@simformsolutions.com>
2023-04-04 18:42:39 +0530

    refectored the code

--- crwmediapart/spiders/media_part.py
+++ crwmediapart/spiders/media_part.py
@@ -171,10 +171,6 @@ class MediaPartSpider(scrapy.Spider, BaseSpider):
                     continue  # If there's any error during the above process, log it and print
         except BaseException as e:
             LOGGER.info(f"Error while parsing sitemap article: {e}")
-            exceptions.SitemapArticleScrappingException(f"Error while parsing sitemap article: {e}")
-
-        except BaseException as e:
-            LOGGER.info(f"Error while parsing sitemap article: {e}")
             raise exceptions.SitemapScrappingException(
                 f"Error while parsing sitemap article: {str(e)}"
             )
@@ -231,8 +227,8 @@ class MediaPartSpider(scrapy.Spider, BaseSpider):
                 self.output_callback(self.articles)
             if not self.articles:
                 LOGGER.info("No articles or sitemap url scrapped.")
-            else:
-                export_data_to_json_file(self.type, self.articles, self.name)
+            # else:
+            #     export_data_to_json_file(self.type, self.articles, self.name)
         except Exception as exception:
             LOGGER.info(f"Error occurred while writing json file{str(exception)} - {reason}")
             raise exceptions.ExportOutputFileException(
