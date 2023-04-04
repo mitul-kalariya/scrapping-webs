01e00bbe167375ce12712dcf685e9e610869841a
Subham Polpagedar <subham.p@simformsolutions.com>
2023-04-04 17:44:44 +0530

    Fixed issue in Test cases

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,10 @@
 from crworientaldaily import Crawler
 
-crawler = Crawler(query={"type": "sitemap"})
-data = crawler.crawl()
+crawler = Crawler(query={"type": "link_feed"})
+links = crawler.crawl()
 
-print(data)
+for link in links[:5]:
+    article = Crawler(query={"type": "article", "link": link["link"]})
+    data = article.crawl()
+    print(data)
+    print("----------------------------------------------------")
