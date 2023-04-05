35cb8856e616ba7a11aef26f3be75b4db287cf11
Subham Polpagedar <subham.p@simformsolutions.com>
2023-04-05 22:40:51 +0530

    Added time_parsed key test case fixes

--- crwbild/utils.py
+++ crwbild/utils.py
@@ -352,6 +352,7 @@ def get_parsed_data(response: str, parsed_json_main: list) -> dict:
     parsed_data_dict |= get_publihser_details(parsed_json_main)
     parsed_data_dict |= get_text_title_section_details(parsed_json_main, response)
     parsed_data_dict |= get_thumbnail_image_video(response)
+    parsed_data_dict |= {"time_parsed": [str(datetime.now())]}
     return remove_empty_elements(parsed_data_dict)
 
 
--- scrapper.py
+++ scrapper.py
@@ -3,4 +3,27 @@ from crwbild.main import Crawler
 crawler = Crawler(query={"type": "sitemap"})
 data = crawler.crawl()
 
-print(data)
+links = [
+    {
+        "url": "https://www.bild.de/bild-plus/regional/hamburg/hamburg-aktuell/techniker-von-kunde-eingesperrt-weil-internet-nicht-lief-83460238.bild.html",
+        "test_data_path": "crwbild/test/data/test_article_1.json"
+    },
+    {
+        "url": "https://www.bild.de/regional/ruhrgebiet/ruhrgebiet-aktuell/umbenennung-nach-116-jahren-uni-muenster-jagt-kaiser-wilhelm-vom-hof-83461618.bild.html",
+        "test_data_path": "crwbild/test/data/test_article_2.json"
+    },
+    {
+        "url": "https://www.bild.de/sport/mehr-sport/handball/wetzlar-trainer-horvat-weg-das-steckt-hinter-der-vertrauensbruch-rauswurf-83454824.bild.html",
+        "test_data_path": "crwbild/test/data/test_article_3.json"
+    }
+]
+
+
+# crawler = Crawler(query={"type": "sitemap"})
+# links = crawler.crawl()
+
+for link in links[:5]:
+    article = Crawler(query={"type": "article", "link": link["url"]})
+    data = article.crawl()
+    print(data)
+    print("----------------------------------------------------")
