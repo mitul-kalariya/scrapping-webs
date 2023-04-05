7b03485b2ba5f9c3485cd7da81e6777e15b1b396
Mitul Kalariya <mitul.k@simformsolutions.com>
2023-04-05 19:59:18 +0530

    merge

--- crwzdfnews/spiders/zdf_news.py
+++ crwzdfnews/spiders/zdf_news.py
@@ -221,9 +221,3 @@ class ZdfNewsSpider(scrapy.Spider, BaseSpider):
         except Exception as exception:
             exceptions.ExportOutputFileException(f"Error occurred while writing json file{str(exception)} - {reason}")
             LOGGER.info(f"Error occurred while writing json file{str(exception)} - {reason}", level=logging.ERROR, )
-
-
-if __name__ == "__main__":
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(ZdfNewsSpider)
-    process.start()
