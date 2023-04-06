bac0c0ba1cd13eb8d02e3eb5ea0e5ee97695e246
Subham Polpagedar <subham.p@simformsolutions.com>
2023-04-06 16:20:37 +0530

    updated scrapper

--- crwhk01/scrapper.py
+++
@@ -1,6 +0,0 @@
-from crwhk01 import Crawler
-
-crawler = Crawler(query={"type": "sitemap"})
-data = crawler.crawl()
-
-print(data)
---
+++ scrapper.py
@@ -0,0 +1,10 @@
+from crwhk01 import Crawler
+
+crawler = Crawler(query={"type": "link_feed"})
+links = crawler.crawl()
+
+for link in links[:5]:
+    article = Crawler(query={"type": "article", "link": link["link"]})
+    data = article.crawl()
+    print(data)
+    print("----------------------------------------------------")
