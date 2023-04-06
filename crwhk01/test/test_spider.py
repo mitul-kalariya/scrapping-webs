a2f2409a170082a87ec0b2bb53dae98b02ba7986
Mihir Vora <mihir.v@simformsolutions.com>
2023-04-06 15:49:26 +0530

    make change in testcases

--- crwhk01/test/test_spider.py
+++ crwhk01/test/test_spider.py
@@ -53,6 +53,21 @@ class TestArticle(unittest.TestCase):
                     self.assertIsInstance(
                         article[0].get("parsed_json").get("misc"), list
                     )
+            with self.subTest():
+                if test_article_data[0].get("parsed_json").get("imageObjects"):
+                    self.assertIsInstance(article[0].get("parsed_json").get("imageObjects"), list,
+                                          "parsed_json --> imageObjects must be list")
+            with self.subTest():
+                if test_article_data[0].get("parsed_json").get("videoObjects"):
+                    self.assertIsInstance(article[0].get("parsed_json").get("videoObjects"), list,
+                                          "parsed_json --> videoObjects must be list")
+            with self.subTest():
+                if test_article_data[0].get("parsed_json").get("other"):
+                    self.assertIsInstance(article[0].get("parsed_json").get("other"), list,
+                                          "parsed_json --> other must be list")
+                if test_article_data[0].get("parsed_json").get("Other"):
+                    self.assertIsInstance(article[0].get("parsed_json").get("other"), list,
+                                          "parsed_json --> other must be list")
 
     def _test_parse_json_with_test_data(self, article, test_article_data):
         # Testing parsed_data object
