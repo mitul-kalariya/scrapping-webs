37d1601fee950e4566858382f90950aa4a11758a
Hanysanghvi18 <hanishajain1435@gmail.com>
2023-04-07 17:02:22 +0530

    change in readme file

--- crwhuffpost/main.py
+++ crwhuffpost/main.py
@@ -75,8 +75,11 @@ class Crawler:
                 "url": self.query.get("link"),
                 "args": {"callback": output_queue.put},
             }
-        elif self.query["type"] == "sitemap":
-            spider_args = {"type": "sitemap", "args": {"callback": output_queue.put}}
+        elif self.query['type'] == 'sitemap':
+            spider_args = {'type': 'sitemap', 'args': {'callback': output_queue.put}}
+            if self.query.get('since') and self.query.get('until'):
+                spider_args['start_date'] = self.query['since']
+                spider_args['end_date'] = self.query['until']
         else:
             raise Exception("Invalid Type")
 
