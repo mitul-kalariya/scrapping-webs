43cc21513e9b959515599040714ed5bc616fc248
mihir <mihirparmar00789@gmail.com>
2023-04-10 12:10:11 +0530

    update according to pylint and flake8

--- crwddnews/spiders/ddnews.py
+++ crwddnews/spiders/ddnews.py
@@ -1,52 +1,77 @@
+"""Spider to scrap DD news website"""
 import logging
-import scrapy
-import w3lib.html
-from datetime import datetime,timedelta,date
+from datetime import datetime, date
 from abc import ABC, abstractmethod
+import scrapy
 from scrapy.loader import ItemLoader
 from crwddnews.items import ArticleData
 from crwddnews import exceptions
-from crwddnews.constant import SITEMAP_URL, LOGGER
-from crwddnews.utils import (create_log_file, validate_sitemap_date_range, get_raw_response, get_parsed_data,
-                              get_parsed_json, export_data_to_json_file, )
+from crwddnews.constant import SITEMAP_URL, LOGGER 
+from crwddnews.utils import (
+    create_log_file,
+    validate_sitemap_date_range,
+    get_raw_response,
+    get_parsed_data,
+    get_parsed_json,
+    export_data_to_json_file,
+)
 
+# create logs
+create_log_file()
 
 
 class BaseSpider(ABC):
+    """Abstract Base class for scrapy spider
+
+    Args:
+        ABC : Abstract
+    """
+
     @abstractmethod
     def parse(self, response):
+        """parse function responsible for calling individual methods for each request"""
         pass
 
     @abstractmethod
     def parse_sitemap(self, response: str) -> None:
+        """called by parse function when response is sitemap"""
         pass
 
     def parse_sitemap_article(self, response: str) -> None:
+        """called by parse function when response is sitemap article"""
         pass
 
     @abstractmethod
     def parse_article(self, response: str) -> list:
+        """called by parse function when response is article"""
         pass
 
 
 class DDNewsSpider(scrapy.Spider, BaseSpider):
+    """main spider for parsing sitemap or article"""
+
     name = "dd_news"
+
     def __init__(self, *args, type=None, url=None, since=None, until=None, **kwargs):
         """
         Initializes a web scraper object to scrape data from a website or sitemap.
         Args:
-            type (str): A string indicating the type of data to scrape. Must be either "sitemap" or "article".
+            type (str): A string indicating the type of data to scrape.
+            Must be either "sitemap" or "article".
             since (str): A string representing the start date of the sitemap to be scraped.
             Must be in the format "YYYY-MM-DD".
             url (str): A string representing the URL of the webpage to be scraped.
             until (str): A string representing the end date of the sitemap to be scraped.
             Must be in the format "YYYY-MM-DD".
-            **kwargs: Additional keyword arguments that can be used to pass information to the web scraper.
+            **kwargs: Additional keyword arguments
+            that can be used to pass information to the web scraper.
         Raises:
             InvalidInputException: If a URL is not provided for an "article" type scraper.
         Notes:
-            This function initializes a web scraper object and sets various properties based on the arguments passed.
-            If the type argument is "sitemap", the start and end dates of the sitemap are validated and set.
+            This function initializes a web scraper object and
+            sets various properties based on the arguments passed.
+            If the type argument is "sitemap", the start and end dates of
+            the sitemap are validated and set.
             If the type argument is "article",
             the URL to be scraped is validated and set. A log file is created for the web scraper.
         """
@@ -61,8 +86,12 @@ class DDNewsSpider(scrapy.Spider, BaseSpider):
             if self.type == "sitemap":
                 self.start_urls.append(SITEMAP_URL)
 
-                self.since = (datetime.strptime(since, "%Y-%m-%d").date() if since else None)
-                self.until = (datetime.strptime(until, "%Y-%m-%d").date() if until else None)
+                self.since = (
+                    datetime.strptime(since, "%Y-%m-%d").date() if since else None
+                )
+                self.until = (
+                    datetime.strptime(until, "%Y-%m-%d").date() if until else None
+                )
 
                 validate_sitemap_date_range(since, until)
 
@@ -74,11 +103,13 @@ class DDNewsSpider(scrapy.Spider, BaseSpider):
                     raise exceptions.InvalidInputException("Must have a URL to scrap")
 
         except Exception as exception:
-            LOGGER.info(f"Error occured in init function in {self.name}:-- {exception}")
+            LOGGER.info(
+                "Error occured in init function in %s:-- %s", self.name, exception
+            )
             raise exceptions.InvalidInputException(
                 f"Error occured in init function in {self.name}:-- {exception}"
             )
-        
+
     def parse(self, response):
         """
         Parses the given Scrapy response based on the specified type of parsing.
@@ -99,11 +130,11 @@ class DDNewsSpider(scrapy.Spider, BaseSpider):
                 yield article_data
 
         except BaseException as e:
-            LOGGER.info(f"Error occured in parse function: {e}")
+            LOGGER.info("Error occured in parse function: %s", e)
             raise exceptions.ParseFunctionFailedException(
                 f"Error occured in parse function: {e}"
             )
-        
+
     def parse_article(self, response) -> list:
         """
         Parses the article data from the response object and returns it as a dictionary.
@@ -136,14 +167,12 @@ class DDNewsSpider(scrapy.Spider, BaseSpider):
 
         except Exception as exception:
             LOGGER.info(
-                f"Error occurred while scrapping an article for this link {response.url}."
-                + str(exception)
+                "Error occurred while scrapping an article for this link %s.",
+                response.url + str(exception),
             )
             raise exceptions.ArticleScrappingException(
                 f"Error occurred while fetching article details:-  {str(exception)}"
             )
-
-
 
     def parse_sitemap(self, response):
         """Parses a sitemap page and extracts links and titles for further processing.
@@ -152,54 +181,83 @@ class DDNewsSpider(scrapy.Spider, BaseSpider):
         Yields:
             scrapy.http.Request: A request object for each link on the sitemap page.
         Raises:
-            exceptions.SitemapScrappingException: If there is an error while parsing the sitemap page.
+            exceptions.SitemapScrappingException: If there is an error while
+            parsing the sitemap page.
         """
         try:
 
             # Get the start and end dates as datetime objects
             since = (
-                datetime.strptime(str(self.since), "%Y-%m-%d")
-                if self.since
-                else None
+                datetime.strptime(str(self.since), "%Y-%m-%d") if self.since else None
             )
             until = (
-                datetime.strptime(str(self.until), "%Y-%m-%d")
-                if self.until
-                else None
+                datetime.strptime(str(self.until), "%Y-%m-%d") if self.until else None
             )
             if since:
                 since = since.strftime("%m/%d/%Y")
                 until = until.strftime("%m/%d/%Y")
                 if since and until:
-                    url = f"https://ddnews.gov.in/about/news-archive?title=&news_type=All&changed_1="+since+"&changed_2="+until
-                    yield scrapy.Request(url, callback=self.parse_archive_pagination_sitemap) 
-                
+                    url = (
+                        "https://ddnews.gov.in/about/news-archive?title=&news_type=All&changed_1="
+                        + since
+                        + "&changed_2="
+                        + until
+                    )
+                    yield scrapy.Request(
+                        url, callback=self.parse_archive_pagination_sitemap
+                    )
+
                 else:
-                    url = f"https://ddnews.gov.in/about/news-archive?title=&news_type=All&changed_1="+since
-                    yield scrapy.Request(url, callback=self.parse_archive_pagination_sitemap) 
-                    
+                    url = (
+                        "https://ddnews.gov.in/about/news-archive?title=&news_type=All&changed_1="
+                        + since
+                    )
+                    yield scrapy.Request(
+                        url, callback=self.parse_archive_pagination_sitemap
+                    )
+
             else:
                 today = date.today().strftime("%m/%d/%Y")
-                url = f"https://ddnews.gov.in/about/news-archive?title=&news_type=All&changed_1="+today
-                yield scrapy.Request(url, callback=self.parse_archive_pagination_sitemap) 
-            
+                url = (
+                    "https://ddnews.gov.in/about/news-archive?title=&news_type=All&changed_1="
+                    + today
+                )
+                yield scrapy.Request(
+                    url, callback=self.parse_archive_pagination_sitemap
+                )
+
         except BaseException as e:
-            LOGGER.info(f"Error while parsing sitemap: {e}")
+            LOGGER.info("Error while parsing sitemap: %s", e)
             raise exceptions.SitemapScrappingException(
                 f"Error while parsing sitemap: {str(e)}"
             )
-        
-    def parse_archive_pagination_sitemap(self,response):
+
+    def parse_archive_pagination_sitemap(self, response):
+        """Parses a archive page and extracts page links and titles for further processing.
+        Args:
+            response (scrapy.http.Response): The HTTP response object containing the archive page.
+        Yields:
+            scrapy.http.Request: A request object for each article link on the page.
+        Raises:
+            exceptions.SitemapScrappingException: If there is an error while
+            parsing the archive page.
+        """
         try:
             pagination_exists = response.css("ul.pager")
             if pagination_exists:
-                total_page = response.css("a[title='Go to last page']::attr(href)").get().split("page=")[1]
-                for page in range(int(total_page)+1):
-                    url = response.url+"&page="+str(page)
-                    yield scrapy.Request(url, callback=self.parse_sitemap_article) 
+                total_page = (
+                    response.css("a[title='Go to last page']::attr(href)")
+                    .get()
+                    .split("page=")[1]
+                )
+                for page in range(int(total_page) + 1):
+                    url = response.url + "&page=" + str(page)
+                    yield scrapy.Request(url, callback=self.parse_sitemap_article)
 
             else:
-                yield scrapy.Request(response.url,dont_filter=True,callback=self.parse_sitemap_article) 
+                yield scrapy.Request(
+                    response.url, dont_filter=True, callback=self.parse_sitemap_article
+                )
         except BaseException as e:
             # LOGGER.info(f"Error while parsing sitemap article: {e}")
             raise exceptions.SitemapArticleScrappingException(
@@ -207,7 +265,8 @@ class DDNewsSpider(scrapy.Spider, BaseSpider):
             )
 
     def parse_sitemap_article(self, response):
-        """Extracts article titles and links from the response object and yields a Scrapy request for each article.
+        """Extracts article titles and links from the response object and
+        yields a Scrapy request for each article.
         Args:
             self: The Scrapy spider instance calling this method.
             response: The response object obtained after making a request to a sitemap URL.
@@ -220,18 +279,14 @@ class DDNewsSpider(scrapy.Spider, BaseSpider):
         try:
             links = response.css("span.field-content a::attr(href)").getall()
             title = response.css("p.archive-title::text").getall()
-            for i,t in zip(links,title):
+            for i, t in zip(links, title):
                 if i and t:
                     self.articles.append(
-                    { 
-                    "link":'https://ddnews.gov.in/'+i,
-                    "title": t
-                    }
+                        {"link": "https://ddnews.gov.in/" + i, "title": t}
+                    )
 
-                )
-                
         except BaseException as e:
-            LOGGER.info(f"Error while parsing sitemap article: {e}")
+            LOGGER.info("Error while parsing sitemap article: %s", e)
             raise exceptions.SitemapArticleScrappingException(
                 f"Error while parsing sitemap article: {str(e)}"
             )
@@ -250,5 +305,9 @@ class DDNewsSpider(scrapy.Spider, BaseSpider):
             else:
                 export_data_to_json_file(self.type, self.articles, self.name)
         except Exception as exception:
-            exceptions.ExportOutputFileException(f"Error occurred while writing json file{str(exception)} - {reason}")
-            LOGGER.info(f"Error occurred while writing json file{str(exception)} - {reason}")
+            exceptions.ExportOutputFileException(
+                "Error occurred while writing json file %s - %s", str(exception), reason
+            )
+            LOGGER.info(
+                "Error occurred while writing json file %s - %s", str(exception), reason
+            )
--- crwddnews/utils.py
+++ crwddnews/utils.py
@@ -1,10 +1,8 @@
-# Utility/helper functions
-# utils.py
-
+"""Utility Function
+"""
 import os
 import re
 import json
-import time
 import logging
 from datetime import datetime
 
@@ -13,31 +11,52 @@ from crwddnews.constant import TODAYS_DATE, LOGGER
 
 
 def create_log_file():
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
-                        filename="logs.log", filemode="a", datefmt="%Y-%m-%d %H:%M:%S", )
+    """creating log file"""
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+        filename="logs.log",
+        filemode="a",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
 
 def validate_sitemap_date_range(start_date, end_date):
-    start_date = datetime.strptime(start_date, "%Y-%m-%d").date() if start_date else None
+    """
+    validating date range given for sitemap
+    """
+    start_date = (
+        datetime.strptime(start_date, "%Y-%m-%d").date() if start_date else None
+    )
     end_date = datetime.strptime(end_date, "%Y-%m-%d").date() if end_date else None
     try:
         if start_date and not end_date:
-            raise exceptions.InvalidDateException("end_date must be specified if start_date is provided")
+            raise exceptions.InvalidDateException(
+                "end_date must be specified if start_date is provided"
+            )
 
         if not start_date and end_date:
-            raise exceptions.InvalidDateException("start_date must be specified if end_date is provided")
+            raise exceptions.InvalidDateException(
+                "start_date must be specified if end_date is provided"
+            )
 
         if start_date and end_date and start_date > end_date:
-            raise exceptions.InvalidDateException("start_date should not be later than end_date")
+            raise exceptions.InvalidDateException(
+                "start_date should not be later than end_date"
+            )
 
         if start_date and end_date and start_date > TODAYS_DATE:
-            raise exceptions.InvalidDateException("start_date should not be greater than today_date")
+            raise exceptions.InvalidDateException(
+                "start_date should not be greater than today_date"
+            )
 
         if start_date and end_date and end_date > TODAYS_DATE:
-            raise exceptions.InvalidDateException("start_date should not be greater than today_date")
+            raise exceptions.InvalidDateException(
+                "start_date should not be greater than today_date"
+            )
 
     except exceptions.InvalidDateException as e:
-        LOGGER.error(f"Error in __init__: {e}", exc_info=True)
+        LOGGER.error("Error in __init__: %s", e, exc_info=True)
         raise exceptions.InvalidDateException(f"Error in __init__: {e}")
 
 
@@ -56,17 +75,31 @@ def remove_empty_elements(parsed_data_dict):
     if not isinstance(parsed_data_dict, (dict, list)):
         data_dict = parsed_data_dict
     elif isinstance(parsed_data_dict, list):
-        data_dict = [value for value in (remove_empty_elements(value) for value in parsed_data_dict) if
-                     not empty(value)]
+        data_dict = [
+            value
+            for value in (remove_empty_elements(value) for value in parsed_data_dict)
+            if not empty(value)
+        ]
     else:
-        data_dict = {key: value for key, value in
-                     ((key, remove_empty_elements(value)) for key, value in parsed_data_dict.items()) if
-                     not empty(value)}
+        data_dict = {
+            key: value
+            for key, value in (
+                (key, remove_empty_elements(value))
+                for key, value in parsed_data_dict.items()
+            )
+            if not empty(value)
+        }
     return data_dict
 
 
 def get_raw_response(response):
-    raw_resopnse = {"content_type": "text/html; charset=utf-8", "content": response.css("html").get(), }
+    """parsing raw response
+    returns: raw response
+    """
+    raw_resopnse = {
+        "content_type": "text/html; charset=utf-8",
+        "content": response.css("html").get(),
+    }
     return raw_resopnse
 
 
@@ -80,13 +113,11 @@ def get_parsed_json(response):
     """
     try:
         parsed_json = {}
-
-
         return parsed_json
     except BaseException as exception:
-        LOGGER.info(f"Error occured while getting parsed json {exception}")
+        LOGGER.info("Error occured while getting parsed json %s", exception)
         raise exceptions.ArticleScrappingException(
-            f"Error occured while getting parsed json {exception}"
+            "Error occured while getting parsed json %s", exception
         ) from exception
 
 
@@ -100,10 +131,8 @@ def get_parsed_data(response):
         dict: returns 2 dictionary parsed_json and parsed_data
     """
     try:
-        pattern = r"[\r\n\t\</h2>\<h2>]+"
-        main_dict = {}
-        main_data = get_main(response)
 
+        main_dict = {}
         topline = response.css("p.heading_small::text").get()
         main_dict["description"] = [topline]
 
@@ -116,10 +145,12 @@ def get_parsed_data(response):
         section = get_section(response)
         if section:
             main_dict["section"] = [section]
-
-
-        display_text = response.css("div.news_content p[class!='heading_small']::text").getall()
-        main_dict["text"] = [" ".join([re.sub("[\r\n\t]+", "", x).strip() for x in display_text])]
+        display_text = response.css(
+            "div.news_content p[class!='heading_small']::text"
+        ).getall()
+        main_dict["text"] = [
+            " ".join([re.sub("[\r\n\t]+", "", x).strip() for x in display_text])
+        ]
 
         images = get_images(response)
         if images:
@@ -132,25 +163,33 @@ def get_parsed_data(response):
         mapper = {"en": "English"}
         article_lang = response.css("html::attr(lang)").get()
         main_dict["source_language"] = [mapper.get(article_lang)]
-
-
         return remove_empty_elements(main_dict)
     except BaseException as e:
-        LOGGER.error(f"while scrapping parsed data {e}")
+        LOGGER.error("while scrapping parsed data %s", e)
         raise exceptions.ArticleScrappingException(f"while scrapping parsed data :{e}")
 
 
 def get_thumbnail(response):
+    """
+    parsing a thumbnail url from the article
+    returns : thumbnial
+    """
     data = get_main(response)
     for data_block in data:
-        if data_block.get('@type') == "WebPage":
-            thumbnail = data_block.get('thumbnailUrl')
+        if data_block.get("@type") == "WebPage":
+            thumbnail = data_block.get("thumbnailUrl")
             if thumbnail:
                 return thumbnail
 
 
 def get_section(response):
-    breadcrumb_list = response.css("div.easy-breadcrumb a.easy-breadcrumb_segment-1::text").getall()
+    """
+    parsing a section(breadcrumb)
+    returns : breaedcrumb list
+    """
+    breadcrumb_list = response.css(
+        "div.easy-breadcrumb a.easy-breadcrumb_segment-1::text"
+    ).getall()
     if breadcrumb_list:
         return breadcrumb_list[-1]
 
@@ -170,11 +209,17 @@ def get_main(response):
             data.append(json.loads(block))
         return data
     except BaseException as e:
-        LOGGER.error(f"error parsing ld+json main data{e}")
-        raise exceptions.ArticleScrappingException(f"error parsing ld+json main data {e}")
+        LOGGER.error("error parsing ld+json main data %s", e)
+        raise exceptions.ArticleScrappingException(
+            f"error parsing ld+json main data {e}"
+        )
 
 
 def get_images(response, parsed_json=False) -> list:
+    """
+    returns a images from the articles
+    returns : Images
+    """
     try:
         images = response.css("span.field-content img")
         pattern = r"[\r\n\t]"
@@ -195,9 +240,8 @@ def get_images(response, parsed_json=False) -> list:
             data.append(temp_dict)
         return data
     except BaseException as e:
-        LOGGER.error(f"image fetching exception {e}")
+        LOGGER.error("image fetching exception %s", e)
         raise exceptions.ArticleScrappingException(f"image fetching exception {e}")
-
 
 
 def export_data_to_json_file(scrape_type: str, file_data: str, file_name: str) -> None:
@@ -216,15 +260,21 @@ def export_data_to_json_file(scrape_type: str, file_data: str, file_name: str) -
         folder_structure = ""
         if scrape_type == "sitemap":
             folder_structure = "Links"
-            filename = f'{file_name}-sitemap-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+            filename = (
+                f'{file_name}-sitemap-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+            )
         elif scrape_type == "article":
             folder_structure = "Article"
-            filename = (f'{file_name}-articles-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+            filename = (
+                f'{file_name}-articles-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+            )
 
         if not os.path.exists(folder_structure):
             os.makedirs(folder_structure)
         with open(f"{folder_structure}/{filename}.json", "w", encoding="utf-8") as file:
             json.dump(file_data, file, indent=4)
     except BaseException as e:
-        LOGGER.error(f"error while creating json file: {e}")
-        raise exceptions.ExportOutputFileException(f"error while creating json file: {e}")
+        LOGGER.error("error while creating json file: %s", e)
+        raise exceptions.ExportOutputFileException(
+            f"error while creating json file: {e}"
+        )
