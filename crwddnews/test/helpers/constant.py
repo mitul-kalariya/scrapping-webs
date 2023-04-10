7da424f69c729f0166d6e669cfddb3e3598d8bdf
Preet Limbani <preet.l@simformsolutions.com>
2023-04-10 10:47:29 +0530

    Updated test constant file

--- crwddnews/test/helpers/constant.py
+++ crwddnews/test/helpers/constant.py
@@ -4,15 +4,16 @@
 TEST_ARTICLES = [
     {
         "url": "https://ddnews.gov.in/international/bangladesh%E2%80%99s-role-vital-materialising-new-vision-free-and-open-indo-pacific-japanese",
-        "test_data_path": "newton-scrapping/crwddnews/test/data/test_article_1.json"
+        "test_data_path": "crwddnews/test/data/test_article_1.json"
     },
     {
         "url": "https://ddnews.gov.in/international/panel-discussion-%E2%80%98women-cinema-uae%E2%80%99-held-dubai",
-        "test_data_path": "newton-scrapping/crwddnews/test/data/test_article_2.json"
+        "test_data_path": ""
+                          "crwddnews/test/data/test_article_2.json"
     },
     {
         "url": "https://ddnews.gov.in/international/bangladesh-receives-450-mw-godda-plant-adani-power",
-        "test_data_path": "newton-scrapping/crwddnews/test/data/test_article_3.json"
+        "test_data_path": "crwddnews/test/data/test_article_3.json"
     }
 ]
 # TODO:Update below sitemap URL
