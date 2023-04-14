7ef23c110120723fe984fcf9f41b7683553eb2fc
tanvi <tanvirupareliya84@gmail.com>
2023-04-14 15:34:35 +0530

    updated test files

--- crwbastillepost/test/helpers/constant.py
+++ crwbastillepost/test/helpers/constant.py
@@ -3,15 +3,15 @@
 # Update below dictionary to add or remove new articles to test
 TEST_ARTICLES = [
     {
-        "url": "https://www.bastillepost.com/hongkong/article/12545585-%e6%b8%af%e7%94%a2%e7%a3%81%e5%8a%9b%e5%85%b1%e6%8c%af%e6%88%90%e5%83%8f%e7%a0%94%e7%99%bc%e6%89%8e%e6%a0%b9%e5%a4%a7%e5%9f%94-%e9%ab%98%e7%ab%af%e7%94%9f%e7%94%a2%e5%8a%a9%e6%b8%af%e6%8e%a8%e5%8b%95",
+        "url": "https://www.bastillepost.com/hongkong/article/12626899-%e8%b3%88%e6%9b%89%e6%99%a8%e6%8b%8d%e7%89%87%e8%ab%87%e5%ae%b6%e5%ba%ad%e7%94%9f%e6%b4%bb%e9%bb%9e%e6%bb%b4-%e5%b1%8b%e4%bc%81%e8%a8%ad%e8%a8%88%e6%9b%9d%e5%85%89%e7%b0%a1%e6%bd%94%e5%8f%88%e9%97%8a",
         "test_data_path": "crwbastillepost/test/data/test_article_1.json"
     },
     {
-        "url": "https://www.bastillepost.com/hongkong/article/12544707-%e8%94%a1%e8%8b%b1%e6%96%87%e5%87%ba%e8%a8%aa%ef%b8%b1%e4%b8%ad%e5%9c%8b%e9%a7%90%e7%b4%90%e7%b4%84%e7%b8%bd%e9%a0%98%e9%a4%a8%ef%bc%9a%e6%89%80%e8%ac%82%e9%81%8e%e5%a2%83%e6%98%af%e5%81%87%e3%80%80",
+        "url": "https://www.bastillepost.com/hongkong/article/12627374-%e6%a9%9f%e5%a0%b4%e4%b8%89%e8%b7%91%e8%b2%aa%e6%b1%a1%e6%a1%88-%e5%bb%89%e7%bd%b2%e5%8a%a0%e6%8e%a7%e4%b8%80%e5%88%86%e5%88%a4%e5%95%86%e6%9d%b1%e4%b8%bb3%e7%bd%aa%e6%b6%89%e8%b3%84%e6%ac%be270",
         "test_data_path": "crwbastillepost/test/data/test_article_2.json"
     },
     {
-        "url": "https://www.bastillepost.com/hongkong/article/12513388-nct-dream%e4%be%86%e6%b8%af%e9%a6%96show%e7%88%86%e8%b7%b33%e5%80%8b%e9%90%98-%e5%85%a8%e5%a0%b4%e9%bd%8a%e8%b3%80%e4%bb%81%e4%bf%8a23%e6%ad%b2%e7%89%9b%e4%b8%80",
+        "url": "https://www.bastillepost.com/hongkong/article/12627413-%e6%b8%af%e8%82%a1%e5%90%91%e4%b8%8a-%e6%99%b6%e7%89%87%e8%82%a1%e5%a4%a7%e5%8d%87-%e9%87%91%e5%b1%ac%e8%b3%87%e6%ba%90%e8%82%a1%e5%90%91%e5%a5%bd",
         "test_data_path": "crwbastillepost/test/data/test_article_3.json"
     }
 ]
