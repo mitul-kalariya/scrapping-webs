c25cc240d9c7f5155fb4e5c2519f4acebb4f77a6
Mitul Kalariya <mitul.k@simformsolutions.com>
2023-04-14 17:01:39 +0530

    right links

--- crwrepublictv/test/helpers/constant.py
+++ crwrepublictv/test/helpers/constant.py
@@ -3,11 +3,11 @@
 # Update below dictionary to add or remove new articles to test
 TEST_ARTICLES = [
     {
-        "url": "https://www.republicworld.com/sports-news/ipl/harbhajan-singh-makes-huge-statement-on-rr-batsman-hes-the-no-dot-1-batter-in-the-world-articleshow.html",
+        "url": "https://bharat.republicworld.com/india-news/general-news/umesh-pal-murder-row-martyr-raghvendra-mother-wants-to-meet-yogi-baba",
         "test_data_path": "crwrepublictv/test/data/test_article_1.json"
     },
     {
-        "url": "https://www.republicworld.com/entertainment-news/bollywood-news/priyanka-chopra-hrithik-roshan-react-to-shocking-events-shared-by-preity-zinta-articleshow.html",
+        "url": "https://bharat.republicworld.com/india-news/general-news/corona-stung-by-china-investigation-accuses-biden-administration-of-doing-politics",
         "test_data_path": "crwrepublictv/test/data/test_article_2.json"
     },
     {
