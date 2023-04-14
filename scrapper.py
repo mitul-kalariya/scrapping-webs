8950439b4054033c2bb9dde0a2e653e934577118
mitang-s-simformsolutions <mitang.s@simformsolutions.com>
2023-04-14 11:00:15 +0530

    updated scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,17 @@
 from crwam730 import Crawler
 
-crawler = Crawler(query={"type": "sitemap"})
-data = crawler.crawl()
+proxies = {
+    "proxyIp": "168.81.229.17",
+    "proxyPort": "3199",
+    "proxyUsername": "",
+    "proxyPassword": "IgNyTnWKr5"
+}
 
-print(data)
+crawler = Crawler(query={"type": "sitemap"})
+links = crawler.crawl()
+
+for link in links[:5]:
+    article = Crawler(query={"type": "article", "link": link["link"]})
+    data = article.crawl()
+    print(data)
+    print("----------------------------------------------------")
