bd33fe2244ac5a2bac557e842e7ee2371f48d653
Dhruvi Makati <dhruvi.m@simformsolutions.com>
2023-04-17 16:29:39 +0530

    updated test data

--- crwbbcnews/constant.py
+++ crwbbcnews/constant.py
@@ -5,5 +5,5 @@ from datetime import datetime
 
 TODAYS_DATE = datetime.today().date()
 SITEMAP_URL = "https://www.bbc.com/zhongwen/simp.json"
-BASE_URL = "https://www.bbc.com"
+BASE_URL = "https://www.bbc.com/zhongwen/simp"
 LOGGER = logging.getLogger()
